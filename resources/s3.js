1ecc8285f6de06efdee29750ff2258f77cdc3316
Armin Graf <arminhammer@gmail.com>
2016-06-19 17:47:46 -0400

    Expanding the embedded types for S3.

--- resources/s3.js
+++ resources/s3.js
@@ -5,19 +5,89 @@
 
 const cloudpotato = require('./../index')
 
+class AmazonS3CorsConfigurationRule extends cloudpotato.SubPropertyObject {
+  constructor(propertiesObject) {
+    let properties = {
+      AllowedHeaders: new cloudpotato.ResourceArray(String, false, null),
+      AllowedMethods: new cloudpotato.ResourceArray(String, true, null),
+      AllowedOrigins: new cloudpotato.ResourceArray(String, true, null),
+      ExposedHeaders: new cloudpotato.ResourceArray(String, false, null),
+      Id: new cloudpotato.ResourceProperty(String, false, null),
+      MaxAge: new cloudpotato.ResourceProperty(Number, false, null)
+    }
+    super(properties, propertiesObject)
+  }
+}
+
+class AmazonS3CorsConfiguration extends cloudpotato.SubPropertyObject {
+  constructor(propertiesObject) {
+    let properties = {
+      CorsRules: new cloudpotato.ResourceArray(AmazonS3CorsConfigurationRule, false, null)
+    }
+    super(properties, propertiesObject)
+  }
+}
+
+class AmazonS3LifecycleRuleNoncurrentVersionTransition extends cloudpotato.SubPropertyObject {
+  constructor(propertiesObject) {
+    let properties = {
+      StorageClass: new cloudpotato.ResourceProperty(String, true, null),
+      TransitionInDays: new cloudpotato.ResourceProperty(Number, true, null)
+    }
+    super(properties, propertiesObject)
+  }
+}
+
+class AmazonS3LifecycleRuleTransition extends cloudpotato.SubPropertyObject {
+  constructor(propertiesObject) {
+    let properties = {
+      StorageClass: new cloudpotato.ResourceProperty(String, true, null),
+      TransitionDate: new cloudpotato.ResourceProperty(String, false, null),
+      TransitionInDays: new cloudpotato.ResourceProperty(Number, false, null)
+    }
+    super(properties, propertiesObject)
+  }
+}
+
+class AmazonS3LifecycleRule extends cloudpotato.SubPropertyObject {
+  constructor(propertiesObject) {
+    let properties = {
+      ExpirationDate: new cloudpotato.ResourceProperty(String, false, null),
+      ExpirationInDays: new cloudpotato.ResourceProperty(Number, false, null),
+      Id: new cloudpotato.ResourceProperty(String, false, null),
+      NoncurrentVersionExpirationInDays: new cloudpotato.ResourceProperty(Number, false, null),
+      NoncurrentVersionTransitions: new cloudpotato.ResourceArray(AmazonS3LifecycleRuleNoncurrentVersionTransition, false, null),
+      Prefix: new cloudpotato.ResourceProperty(String, false, null),
+      Status: new cloudpotato.ResourceProperty(String, false, null),
+      Transitions: new cloudpotato.ResourceArray(AmazonS3LifecycleRuleTransition, false, null)
+    }
+    super(properties, propertiesObject)
+  }
+}
+
+class AmazonS3LoggingConfiguration extends cloudpotato.SubPropertyObject {
+  constructor(propertiesObject) {
+    let properties = {
+      DestinationBucketName: new cloudpotato.ResourceProperty(String, false, null),
+      LogFilePrefix: new cloudpotato.ResourceProperty(String, false, null)
+    }
+    super(properties, propertiesObject)
+  }
+}
+
 class Bucket extends cloudpotato.BaseAWSObject {
   constructor (name, propertiesObject) {
     let resourceType = 'AWS::S3::Bucket'
     let properties = {
       AccessControl: new cloudpotato.ResourceProperty(String, false, null),
-      BucketName: new cloudpotato.ResourceProperty(String, false, null)
-      /* CorsConfiguration : CORS Configuration,
-      LifecycleConfiguration : Lifecycle Configuration,
-      LoggingConfiguration : Logging Configuration,
-      NotificationConfiguration : Notification Configuration,
-      ReplicationConfiguration : Replication Configuration,
-      Tags : [ Resource Tag, ... ],
-      VersioningConfiguration : Versioning Configuration,
+      BucketName: new cloudpotato.ResourceProperty(String, false, null),
+      CorsConfiguration: new cloudpotato.ResourceProperty(AmazonS3CorsConfiguration, false, null),
+      LifecycleConfiguration: new cloudpotato.ResourceProperty(AmazonS3LifecycleRule, false, null),
+      LoggingConfiguration: new cloudpotato.ResourceProperty(AmazonS3LoggingConfiguration, false, null),
+      /* NotificationConfiguration : Notification Configuration,
+      ReplicationConfiguration : Replication Configuration,*/
+       Tags: new cloudpotato.TagSet(),
+      /* VersioningConfiguration : Versioning Configuration,
       WebsiteConfiguration : Website Configuration Type*/
     }
     super(name, resourceType, properties, propertiesObject)
@@ -36,6 +106,12 @@ class BucketPolicy extends cloudpotato.BaseAWSObject {
 }
 
 module.exports = {
+  AmazonS3CorsConfigurationRule: AmazonS3CorsConfigurationRule,
+  AmazonS3CorsConfiguration: AmazonS3CorsConfiguration,
+  AmazonS3LifecycleRuleNoncurrentVersionTransition: AmazonS3LifecycleRuleNoncurrentVersionTransition,
+  AmazonS3LifecycleRuleTransition: AmazonS3LifecycleRuleTransition,
+  AmazonS3LifecycleRule: AmazonS3LifecycleRule,
+  AmazonS3LoggingConfiguration: AmazonS3LoggingConfiguration,
   Bucket: Bucket,
   BucketPolicy: BucketPolicy
 }
