7d473ec85c39e5076bdf36efef71e367de547289
Armin Graf <arminhammer@gmail.com>
2016-06-20 22:51:08 -0400

    Added support for events.

--- resources/events.js
+++ resources/events.js
@@ -5,6 +5,22 @@
 
 const cloudpotato = require('./../index')
 
+class Rule extends cloudpotato.BaseAWSObject {
+  constructor (name, propertiesObject) {
+    let resourceType = 'AWS::Events::Rule'
+    let properties = {
+      Description: new cloudpotato.ResourceProperty(String, false, null),
+      EventPattern: new cloudpotato.ResourceProperty(Object, false, null),
+      Name: new cloudpotato.ResourceProperty(String, false, null),
+      RoleArn: new cloudpotato.ResourceProperty(String, false, null),
+      ScheduleExpression: new cloudpotato.ResourceProperty(String, false, null),
+      State: new cloudpotato.ResourceProperty(String, false, null),
+      Targets: new cloudpotato.ResourceArray(String, false, null),
+    }
+    super(name, resourceType, properties, propertiesObject)
+  }
+}
+
 module.exports = {
-  AWS::Events::Rule
+  Rule: Rule
 }
