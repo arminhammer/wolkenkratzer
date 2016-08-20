dad759368654a072b589ad973856afc4178a5445
Armin Graf <arminhammer@gmail.com>
2016-08-20 17:57:52 -0400

    Changed intrinsic classes to use function() instead of class.

--- intrinsic.js
+++ intrinsic.js
@@ -11,128 +11,111 @@ const Parameter = require('./parameter').Parameter
 /**
  * @memberof module:Core
  */
-class Intrinsic {
-  /**
-   * Returns a JSON Object
-   */
-  toJson () {}
+function Intrinsic () {
 }
+
+/**
+ * Returns a JSON Object
+ */
+Intrinsic.prototype.toJson = function () {}
 
 /**
  * @memberof module:Core
  */
-class Ref extends Intrinsic {
-  /**
-   * @constructs Ref
-   * @param resource
-   */
-  constructor (resource) {
-    super()
-    this.ref = resource
-  }
+function Ref (resource) {
+  Intrinsic.call(this)
+  this.ref = resource
+}
+Ref.prototype = Object.create(Intrinsic.prototype)
 
-  /**
-   * Returns a JSON string version
-   * @returns {Object}
-   */
-  toJson () {
+/**
+ * Returns a JSON string version
+ * @returns {Object}
+ */
+Ref.prototype.toJson = function () {
+  return { 'Ref': this.ref.WKName }
+}
+
+Ref.prototype.toJSON = function () {
+  if (this.ref instanceof WKResource || this.ref instanceof Parameter) {
     return { 'Ref': this.ref.WKName }
-  }
-  toJSON () {
-    if (this.ref instanceof WKResource || this.ref instanceof Parameter) {
-      return { 'Ref': this.ref.WKName }
-    } else {
-      return { 'Ref': this.ref }
-    }
+  } else {
+    return { 'Ref': this.ref }
   }
 }
 
 /**
  * @memberof module:Core
  */
-class FnGetAtt extends Intrinsic {
-  /**
-   * @constructs FnGetAtt
-   * @param resource
-   * @param attribute
-   */
-  constructor (resource, attribute) {
-    super()
-    this.resource = resource
-    this.attribute = attribute
-  }
+function FnGetAtt (resource, attribute) {
+  Intrinsic.call(this)
+  this.resource = resource
+  this.attribute = attribute
+}
+FnGetAtt.prototype = Object.create(Intrinsic.prototype)
 
-  /**
-   * Returns a JSON string version
-   * @returns {Object}
-   */
-  toJson () {
+/**
+ * Returns a JSON string version
+ * @returns {Object}
+ */
+FnGetAtt.prototype.toJson = function () {
+  return { 'Fn::GetAtt': [this.resource.WKName, this.attribute] }
+}
+
+FnGetAtt.prototype.toJSON = function () {
+  if (this.resource instanceof WKResource || this.resource instanceof Parameter) {
     return { 'Fn::GetAtt': [this.resource.WKName, this.attribute] }
-  }
-  toJSON () {
-    if (this.resource instanceof WKResource || this.resource instanceof Parameter) {
-      return { 'Fn::GetAtt': [this.resource.WKName, this.attribute] }
-    } else {
-      return { 'Fn::GetAtt': [this.resource, this.attribute] }
-    }
+  } else {
+    return { 'Fn::GetAtt': [this.resource, this.attribute] }
   }
 }
 
 /**
  * @memberof module:Core
  */
-class FnBase64 extends Intrinsic {
-  /**
-   * @constructs FnBase64
-   * @param content
-   */
-  constructor (content) {
-    super()
-    this.content = content
-  }
-
-  /**
-   * Returns a JSON string version
-   * @returns {Object}
-   */
-  toJson () {
-    return { 'Fn::Base64': this.content }
-  }
-  toJSON () {
-    return { 'Fn::Base64': this.content }
-  }
+function FnBase64 (content) {
+  Intrinsic.call(this)
+  this.content = content
 }
+FnBase64.prototype = Object.create(Intrinsic.prototype)
+
+/**
+ * Returns a JSON string version
+ * @returns {Object}
+ */
+FnBase64.prototype.toJson = function () {
+  return { 'Fn::Base64': this.content }
+}
+
+FnBase64.prototype.toJSON = function () {
+  return { 'Fn::Base64': this.content }
+}
+
 
 /**
  * @memberof module:Core
  */
-class FnFindInMap extends Intrinsic {
-  /**
-   * @constructs FnFindInMap
-   * @param mapName
-   * @param topLevelKey
-   * @param secondLevelKey
-   */
-  constructor (mapName, topLevelKey, secondLevelKey) {
-    super()
-    this.mapName = mapName
-    this.topLevelKey = topLevelKey
-    this.secondLevelKey = secondLevelKey
-  }
+function FnFindInMap (mapName, topLevelKey, secondLevelKey) {
+  Intrinsic.call(this)
+  this.mapName = mapName
+  this.topLevelKey = topLevelKey
+  this.secondLevelKey = secondLevelKey
+}
+FnFindInMap.prototype = Object.create(Intrinsic.prototype)
 
-  /**
-   * Returns a JSON string version
-   * @returns {Object}
-   */
-  toJson () {
-    return { 'Fn::FindInMap': [ this.mapName, this.topLevelKey, this.secondLevelKey ] }
-  }
-  toJSON () {
-    return { 'Fn::FindInMap': [ this.mapName, this.topLevelKey, this.secondLevelKey ] }
-  }
+/**
+ * Returns a JSON string version
+ * @returns {Object}
+ */
+FnFindInMap.prototype.toJson = function () {
+  return { 'Fn::FindInMap': [ this.mapName, this.topLevelKey, this.secondLevelKey ] }
 }
 
-/* class FnGetAZs extends Intrinsic {
+FnFindInMap.prototype.toJSON = function () {
+  return { 'Fn::FindInMap': [ this.mapName, this.topLevelKey, this.secondLevelKey ] }
+}
+
+/* function FnGetAZs extends Intrinsic {
  constructor (region) {
  super()
  this.region = region
@@ -143,31 +126,25 @@ class FnFindInMap extends Intrinsic {
 /**
  * @memberof module:Core
  */
-class FnJoin extends Intrinsic {
-  /**
-   * @constructs FnJoin
-   * @param delimiter
-   * @param values
-   */
-  constructor (delimiter, values) {
-    super()
-    this.delimiter = delimiter
-    this.values = values
-  }
-
-  /**
-   * Returns a JSON string version
-   * @returns {Object}
-   */
-  toJson () {
-    return { 'Fn::Join': [ this.delimiter, this.values ] }
-  }
-  toJSON () {
-    return { 'Fn::Join': [ this.delimiter, this.values ] }
-  }
+function FnJoin (delimiter, values) {
+  Intrinsic.call(this)
+  this.delimiter = delimiter
+  this.values = values
+}
+FnJoin.prototype = Object.create(Intrinsic.prototype)
+/**
+ * Returns a JSON string version
+ * @returns {Object}
+ */
+FnJoin.prototype.toJson = function () {
+  return { 'Fn::Join': [ this.delimiter, this.values ] }
 }
 
-/* class FnSelect extends Intrinsic {
+FnJoin.prototype.toJSON = function () {
+  return { 'Fn::Join': [ this.delimiter, this.values ] }
+}
+
+/* function FnSelect extends Intrinsic {
  constructor (index, list) {
  super()
  this.index = index
@@ -176,35 +153,35 @@ class FnJoin extends Intrinsic {
  toJson () {}
  }*/
 
-/* class Conditional extends Intrinsic {
+/* function Conditional extends Intrinsic {
  constructor () {
  super()
  }
  toJson () {}
  }*/
 
-/* class FnIf extends Conditional {
+/* function FnIf extends Conditional {
  constructor () {
  super()
  }
  toJson () {}
  }*/
 
-/* class FnEquals extends Conditional {
+/* function FnEquals extends Conditional {
  constructor () {
  super()
  }
  toJson () {}
  }*/
 
-/* class FnNot extends Conditional {
+/* function FnNot extends Conditional {
  constructor () {
  super()
  }
  toJson () {}
  }*/
 
-/* class FnOr extends Conditional {
+/* function FnOr extends Conditional {
  constructor () {
  super()
  }
