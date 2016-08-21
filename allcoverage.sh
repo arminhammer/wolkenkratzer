202aa9b2a69886f1cca039374e17ad4c402e4cae
Armin Graf <arminhammer@gmail.com>
2016-08-21 00:44:24 -0400

    Fixing coverage logic, upping to 0.4.0.

--- allcoverage.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-istanbul cover --print detail ./node_modules/.bin/_mocha 'test/**/*.spec.js' --dir ./coverage/tests
-istanbul cover --print detail ./node_modules/.bin/_mocha 'examples/*.js' --dir ./coverage/examples
-istanbul report
