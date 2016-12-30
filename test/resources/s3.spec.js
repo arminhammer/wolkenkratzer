1dadf332de6d7a609db870d487ac599b57661f51
Armin Graf <arminhammer@gmail.com>
2016-12-30 14:56:36 -0500

    Overriding array status for AmazonS3NotificationConfiguration. Updated unit tests.

--- test/resources/s3.spec.js
+++ test/resources/s3.spec.js
@@ -74,6 +74,47 @@ describe('S3', () => {
     })
   })
 
+  it('should allow for Lambda Notifications to be added to a Bucket', () => {
+
+    let intermediateBucketName = 'bucketName'
+
+    let TestLambdaFunction = new wk.Lambda.Function('TestLambdaFunction')
+    TestLambdaFunction.Handler = 'index.handler'
+    // TestLambdaFunction.Role.getAtt(LambdaExecutionRole, 'Arn')
+    TestLambdaFunction.Runtime = 'nodejs4.3'
+    t.add(TestLambdaFunction)
+
+    let BucketPermission = new wk.Lambda.Permission('BucketPermission')
+    BucketPermission.FunctionName.ref(TestLambdaFunction)
+    BucketPermission.Action = 'lambda:InvokeFunction'
+    BucketPermission.Principal = 's3.amazonaws.com'
+    // TODO: Fix SourceAccount issue
+    BucketPermission.SourceAccount = new wk.Intrinsic.Ref(wk.Pseudo.AWS_ACCOUNT_ID)
+    BucketPermission.SourceArn = new wk.Intrinsic.FnJoin(':', ['arn', 'aws', 's3', '' , '', intermediateBucketName])
+    t.add(BucketPermission)
+
+    let ssmintermediatebucket = new wk.S3.Bucket('testbucket')
+    ssmintermediatebucket.dependsOn(BucketPermission)
+    ssmintermediatebucket.BucketName = intermediateBucketName
+    let bucketNotifications = new wk.Types.AmazonS3NotificationConfiguration()
+    let lambdaNotification = new wk.Types.AmazonSimpleStorageServiceNotificationConfigurationLambdaConfigurations()
+    lambdaNotification.Event = 's3:ObjectCreated:Put'
+    lambdaNotification.Function = new wk.Intrinsic.FnGetAtt(TestLambdaFunction, 'Arn')
+    let filter = new wk.Types.AmazonS3NotificationConfigurationConfigFilter()
+    let s3KeyRule = new wk.Types.AmazonS3NotificationConfigurationConfigFilterS3KeyRules()
+    s3KeyRule.Name = 'Suffix'
+    s3KeyRule.Value = 'stdout'
+    let s3Key = new wk.Types.AmazonS3NotificationConfigurationConfigFilterS3Key()
+    s3Key.Rules.push(s3KeyRule)
+    filter.S3Key = s3Key
+    lambdaNotification.Filter = filter
+    bucketNotifications.LambdaConfigurations.push(lambdaNotification)
+    ssmintermediatebucket.NotificationConfiguration = bucketNotifications
+
+    t.add(ssmintermediatebucket)
+
+  })
+
   it ('CloudFormation should validate the template NetworkTest', (done) => {
     util.validateTemplate(t, done)
   })
