d855fa576df7438eba0228ca3b0f8207daa6e70a
arminhammer <armin.graf@capitalone.com>
2017-09-02 19:36:18 -0400

    Adding support for UpdatePolicy

--- dist/elements/resource.d.ts
+++ dist/elements/resource.d.ts
@@ -1,6 +1,7 @@
 import { ICreationPolicy } from '../attributes/creationpolicy';
 import { IDeletionPolicy } from '../attributes/deletionpolicy';
 import { IDependsOn } from '../attributes/dependson';
+import { IUpdatePolicy } from '../attributes/updatepolicy';
 export interface IResource {
     readonly kind: 'Resource';
     readonly Name: string;
@@ -11,6 +12,7 @@ export interface IResource {
     readonly CreationPolicy?: ICreationPolicy;
     readonly DeletionPolicy?: IDeletionPolicy;
     readonly DependsOn?: IDependsOn;
+    readonly UpdatePolicy?: IUpdatePolicy;
 }
 /**
  * Create a Resource object
