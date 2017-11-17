791281868272765733f22c1e83951380e805fa47
arminhammer <arminhammer@gmail.com>
2017-11-16 20:21:45 -0500

    Making function hidden

--- src/template.ts
+++ src/template.ts
@@ -326,6 +326,10 @@ export function Template(): ITemplate {
   };
 }
 
+/**
+ * @hidden
+ * @param obj
+ */
 function _isEmptyObject(obj) {
   return Object.keys(obj).length === 0 && obj.constructor === Object;
 }
